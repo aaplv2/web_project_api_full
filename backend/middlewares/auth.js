6c52576cd7f8b7fb8c94fef353cae10318fc7b0a
Andres <aaplv2@gmail.com>
2024-09-06 13:51:19 -0400

    middleware auth

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,24 @@
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if(!authorization || !authorization.startsWith('Bearer')) {
+    return res
+    .status(401)
+    .send({ message: 'Se requiere autorización' })
+  }
+
+  const token = authorization.replace('Bearer', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'clave-secreta');
+  } catch (err) {
+    return res
+    .status(401)
+    .send({ message: 'Se requiere autorización' })
+  }
+
+  req.user = payload;
+
+  next();
+};
