d30790cd237a4ce33c0f563dc77a593a3a9fe5c1
Andres <aaplv2@gmail.com>
2024-09-13 18:06:47 -0300

    req.user

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -16,6 +16,7 @@ module.exports.getCards = (req, res, next) => {
 
 module.exports.createCard = (req, res, next) => {
   const { name, link, owner } = req.body;
+  console.log(req.user)
   Card.create({ title: name, link, owner: req.user })
     .then((card) => {
       res.send({ data: card });
